f8bb06eff54f0a408caebf2c79116de6804363cd
DiegoLopez <dzedolc27@gmail.com>
2023-03-13 11:26:22 -0700

    test

---
+++ settings.py
@@ -0,0 +1,20 @@
+# File created by: Diego Lopez
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+#player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
+MOB_ACC = 2
+MOB_FRICTION = -0.12
+
+#colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,0,0)
