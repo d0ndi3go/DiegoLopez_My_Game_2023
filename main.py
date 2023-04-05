d47c3952dd3d2c38661311c1b9bbc965f9064b89
d0ndi3go <dzedolc27@gmail.com>
2023-04-05 10:50:27 -0700

    test

--- main.py
+++ main.py
@@ -7,46 +7,59 @@
 #
 # import libs
 import pygame as pg
-import random
 import os
-
-# need this to create file paths effectively
-from os import path
-
-# import settings
+# import settings 
 from settings import *
 from sprites import *
-from random import randint
+from math import *
+from math import ceil
+from os import path
 # from pg.sprite import Sprite
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "images")
 
-# create the game class to organize game content better...
+# create game class in order to pass properties to the sprites file
+
 class Game:
-    # inits the pygame stuff including setting up screen/display area
     def __init__(self):
+        # init game window etc.
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("My Game...")
+        pg.display.set_caption("my game")
         self.clock = pg.time.Clock()
         self.running = True
-    # method to create a new game...
+        print(self.screen)
+    
+    def load_data(self):
+        self.player_img = pg.image.load(path.join(img_folder, "bellar_man_single_frm.png")).convert()
+
     def new(self):
-            self.score = 0
-            self.all_sprites = pg.sprite.Group()
-            self.platforms = pg.sprite.Group()
-            self.enemies = pg.sprite.Group()
-            # instantiates player class from sprites file, and passes this game class as
-            # an argument
-            self.player = Player(self)
-            self.all_sprites.add(self.player)
-            for i in range(1,10):
-                e = Mob()
-                self.all_sprites.add(e)
-            self.run()  
+        # starting a new game
+        self.score = 0
+        self.load_data()
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        # self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        for i in range(0,10):
+            m = Mob(self, 20,20,(0,255,0))
+            self.all_sprites.add(m)
+            self.enemies.add(m)
+        self.run()
     def run(self):
         self.playing = True
         while self.playing:
@@ -54,10 +67,9 @@ class Game:
             self.events()
             self.update()
             self.draw()
+    
     def events(self):
-        # Game Loop - events
         for event in pg.event.get():
-            # check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
@@ -65,30 +77,47 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump()
+    def update(self):
+        self.all_sprites.update()
+        mhits = pg.sprite.spritecollide(self.player, self.enemies, False)
+        if mhits:
+            if abs(self.player.vel.x) > abs(self.player.vel.y):
+                self.player.vel.x *= -1
+            else:
+                self.player.vel.y *= -1
+            
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+    def draw(self):
+        self.screen.fill(BLUE)
+        self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.player.rot), 24, WHITE, WIDTH/2, HEIGHT/2)
+        # is this a method or a function?
+        pg.display.flip()
     def draw_text(self, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
+        text_rect.midtop = (x,y)
         self.screen.blit(text_surface, text_rect)
-    # def get_mouse_now():
-    #     x,y = pg.mouse.get_pos()
-    #     return (x,y)
-    def update(self):
-        self.all_sprites.update()
-    def draw(self):
-        self.screen.fill(BLUE)
-        self.draw_text("Hello there!", 42, WHITE, WIDTH/2, HEIGHT/10)
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)
 
-vec = pg.math.Vector2
-
-# game loop
-
+# instantiate the game class...
 g = Game()
 
+# kick off the game loop
 while g.running:
     g.new()
 
--- sprites.py
+++ sprites.py
@@ -1,80 +1,165 @@
 # File created by: Diego Lopez
 
 import pygame as pg
-
 from pygame.sprite import Sprite
-
 from settings import *
-
 from random import randint
+from random import randrange
 
 vec = pg.math.Vector2
 
-
-# create a player
+# player class
 
 class Player(Sprite):
     def __init__(self, game):
         Sprite.__init__(self)
+        # these are the properties
         self.game = game
-        self.image = pg.Surface((50,50))
-        # self.image = pg.transform.scale((50, 38))
-        self.image.fill(BLACK)
+        self.image_orig = pg.transform.scale(game.player_img, (128, 128))
+        self.image_orig.set_colorkey(WHITE)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.radius = int(self.rect.width * .85 / 2)
+        self.pos = vec(0, 0)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
+        self.rot = 0
+        self.rot_speed = 0
+        self.last_update = pg.time.get_ticks()
+        self.left_key = pg.K_a
     def input(self):
         keystate = pg.key.get_pressed()
-        if keystate[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
-        if keystate[pg.K_a]:
+        # if keystate[pg.K_w]:
+        #     self.acc.y = -PLAYER_ACC
+        if keystate[self.left_key]:
             self.acc.x = -PLAYER_ACC
-        if keystate[pg.K_s]:
-            self.acc.y = PLAYER_ACC
+            self.rot_speed = 8
+        # if keystate[pg.K_s]:
+        #     self.acc.y = PLAYER_ACC
         if keystate[pg.K_d]:
             self.acc.x = PLAYER_ACC
-    # def jump(self):
-    #     # jump only if standing on a platform
-    #     self.rect.x += 1
-    #     hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-    #     self.rect.x -= 1
-    #     if hits:
-    #         self.vel.y = -PLAYER_JUMP
+            self.rot_speed = -8
+        # if keystate[pg.K_p]:
+        #     if PAUSED == False:
+        #         PAUSED = True
+        #         print(PAUSED)
+        #     else:
+        #         PAUSED = False
+        #         print(PAUSED)
+    # ...
+    def rotate(self):
+        now = pg.time.get_ticks()
+        if now - self.last_update > 30:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pg.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+    def jump(self):
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits and self.canjump:
+            self.vel.y = -PLAYER_JUMP
+    
+    def inbounds(self):
+        if self.rect.x > WIDTH - 50:
+            self.pos.x = WIDTH - 25
+            self.vel.x = 0
+            print("i am off the right side of the screen...")
+        if self.rect.x < 0:
+            self.pos.x = 25
+            self.vel.x = 0
+            print("i am off the left side of the screen...")
+        if self.rect.y > HEIGHT:
+            print("i am off the bottom of the screen")
+        if self.rect.y < 0:
+            print("i am off the top of the screen...")
+    def mob_collide(self):
+            hits = pg.sprite.spritecollide(self, self.game.enemies, True)
+            if hits:
+                print("you collided with an enemy...")
+                self.game.score += 1
+                print(SCORE)
     def update(self):
-        self.acc = self.vel * PLAYER_FRICTION
+        self.rot_speed = 0
+        if self.rot > 312 or self.rot < 56:
+            self.canjump = True
+        else:
+            self.canjump = False
+        self.acc = vec(0, PLAYER_GRAV)
+        self.acc.x = self.vel.x * PLAYER_FRICTION
         self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-        self.rect.center = self.pos
-        if self.rect.x > WIDTH:
-            print("I'm off the right screen...")
-        if self.rect.x < 0:
-            print("I'm off the left screen...")
-        if self.rect.y < 0:
-            print("I'm off the top screen...")
-        if self.rect.y > HEIGHT:
-            print("I'm off the bottom screen...")
+        self.rotate()
+        self.rect.midbottom = self.pos
 
 class Mob(Sprite):
-    def __init__(self):
+    def __init__(self, game, width,height, color):
         Sprite.__init__(self)
-        self.image = pg.Surface((50,50))
-        self.image.fill(RED)
+        self.game = game
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(randint(1,5),randint(1,5))
-        self.acc = vec(0,0)
-        self.cofric = 0.1
-        self.canjump = False
-    def behavior(self):
-        if self.rect.x > WIDTH or self.rect.x < 0 or self.rect.y > HEIGHT or self.rect.y < 0:
-            self.vel *= -1
-
+        # self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(100, 100)
+        self.vel = vec(0,0)
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+        print(self.vel.x)
+        print(self.vel.y)
+    # ...
+    def jump(self):
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        if hits:
+            if self.vel.y > 1:
+                self.vel = vec(5,-10)
+                print(self.vel.y)
+            else:
+                self.pos.y = hits[0].rect.top
+                self.vel.y = 0
+        else:
+            self.vel.x = 0
+    def inbounds(self):
+        if self.rect.x > WIDTH:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.x < 0:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y < 0:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y > HEIGHT:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
     def update(self):
-        self.behavior()
-        self.pos += self.vel
-        self.rect.center = self.pos
-        
+        self.acc = vec(0, PLAYER_GRAV)
+        self.jump()
+        self.acc.x = self.vel.x * PLAYER_FRICTION
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.midbottom = self.pos
+
+# create a new platform class...
+
+class Platform(Sprite):
+    def __init__(self, x, y, width, height, color, variant):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.variant = variant
